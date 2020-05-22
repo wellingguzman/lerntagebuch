1ab81575207395531b6052cb57c0a438b658a94c
Welling Guzman <welling.guzman@instafreight.com>
2020-05-22 19:32:09 +0200

    first commit

---
+++ b.sh
@@ -0,0 +1,363 @@
+#!/usr/bin/env bash
+
+g_root=$(pwd)
+g_posts_path="$g_root/posts"
+g_build_path="$g_root/public"
+g_site_title="Welling Guzman's log"
+g_site_description="Infrequently learning journal and random notes"
+g_markdown="$g_root/vendor/Markdown.pls"
+
+g_PATH_INFO_NAME=0
+g_PATH_INFO_EXT=1
+g_PATH_INFO_BASENAME=2
+
+g_POST_PATH=0
+g_POST_TITLE=1
+g_POST_DATETIME=2
+g_POST_TAGS=3
+g_POST_CONTENT=4
+
+get_ext()
+{
+	path=$1
+	echo "${path#*.}"
+}
+
+get_name()
+{
+	path=$1
+	ext=$(get_ext $path)
+	name=$(basename "${path%.$ext}")
+
+	echo $name
+}
+
+get_basename()
+{
+	path=$1
+	echo ${path##*/}
+}
+
+get_path_info()
+{
+	local name=$(get_name "$1")
+	local ext=$(get_ext "$1")
+	local basename=$name
+	if [[ -z "$ext" ]]; then
+		basename+=".$ext"
+	fi
+
+	pathinfo=(
+		"$name"
+		"$ext"
+		"$basename"
+	)
+}
+
+get_content()
+{
+	path=$1
+	cat $path
+}
+
+convert_content()
+{
+	content=$1
+
+	if [[ -f "$g_markdown" ]]; then
+		echo "$1" | $g_markdown
+	else
+		echo "$1"
+	fi
+}
+
+get_file_parts()
+{
+	local path=$1
+	local title=""
+	local tags=""
+	local datetime=""
+	local content=""
+	local tmp="$path.info.tmp"
+
+	while IFS= read -r line
+	do
+		if [[ $line == "title:"* ]]; then
+			title=$(echo $line | cut -d: -f 2 | awk '{$1=$1};1')
+		elif [[ $line == "tags:"* ]]; then
+			tags=$(echo $line | cut -d: -f 2 | awk '{$1=$1};1')
+		elif [[ $line == "datetime:"* ]]; then
+			datetime=$(echo $line | cut -d: -f 2- | awk '{$1=$1};1')
+		else
+			echo "$line" >> "$tmp"
+		fi
+	done < "$path"
+
+	content=$(cat "$tmp")
+	rm -rf "$tmp"
+
+	parts=(
+		"$path"
+		"$title"
+		"$datetime"
+		"$tags"
+		"$content"
+	)
+}
+
+build_page()
+{
+	content=$1
+	if [[ ! -z "$title" ]]; then
+		title="$title – $g_site_title"
+	else
+		title=$g_site_title
+	fi
+
+	echo "<!doctype html>" >> "$tmp"
+	echo "<html lang=\"en\">" >> "$tmp"
+	echo "<head>" >> "$tmp"
+	echo "<meta charset=\"utf-8\">" >> "$tmp"
+	echo "<title>$title</title>" >> "$tmp"
+	if [[ ! -z $g_site_description ]]; then
+		echo "<meta name=\"description\" content=\"$g_site_description\">" >> "$tmp"
+	fi
+	echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">" >> "$tmp"
+	echo "</head>" >> "$tmp"
+
+	local styles_path="$g_root/_styles.css"
+	if [[ -f "$styles_path" ]]; then
+		styles=$(get_content "$styles_path")
+		echo "<style>" >> "$tmp"
+		echo $styles >> "$tmp"
+		echo "</style>" >> "$tmp"
+	fi
+
+	echo "</head>" >> "$tmp"
+	echo "<body>" >> "$tmp"
+	echo "<div class=\"container\">" >> "$tmp"
+	echo "<header>" >> "$tmp"
+	echo "<h1><a href=\"/\">$g_site_title</a></h1>" >> "$tmp"
+	echo "<p>$g_site_description</p>" >> "$tmp"
+	echo "</header>" >> "$tmp"
+	echo "$content" >> "$tmp"
+
+	echo "</div>" >> "$tmp"
+	echo "</body></html>" >> "$tmp"
+}
+
+create_post()
+{
+	path=$1
+	content=$(get_content $path)
+	get_path_info "$path"
+	ext="${pathinfo[g_PATH_INFO_EXT]}"
+	name=$(get_name $path)
+
+	tmp="$path.tmp"
+	title=""
+	tags=""
+	datetime=""
+	content=""
+
+	echo "Building $name..."
+	echo "Reading metadata..."
+	while IFS= read -r line
+	do
+		if [[ $line == "title:"* ]]; then
+			title=$(echo $line | cut -d: -f 2 | awk '{$1=$1};1')
+		elif [[ $line == "tags:"* ]]; then
+			tags=$(echo $line | cut -d: -f 2 | awk '{$1=$1};1')
+		elif [[ $line == "datetime:"* ]]; then
+			datetime=$(echo $line | cut -d: -f 2- | awk '{$1=$1};1')
+		else
+			echo "$line" >> "$tmp"
+		fi
+	done < "$path"
+
+	content=$(cat $tmp)
+	if [[ $ext == "md" ]]; then
+		echo "Converting to HTML..."
+		content=$(convert_content "$content")
+	fi
+
+	>$tmp
+	echo "<article>" >> "$tmp"
+	echo "<h1 class=\"title\">$title</h1>" >> "$tmp"
+	echo "$content" >> "$tmp"
+	
+	if [[ ! -z "$datetime" ]] || [[ ! -z "$tags" ]]; then
+		echo "<div class=\"meta\">" >> "$tmp"
+	fi
+	if [[ ! -z "$datetime" ]]; then
+		echo "Date: <time>$datetime</time>" >> "$tmp"
+	fi
+	if [[ ! -z "$tags" ]]; then
+		echo $(get_tags "$tags") >> "$tmp"
+	fi
+	if [[ ! -z "$datetime" ]] || [[ ! -z "$tags" ]]; then
+		echo "</div>" >> "$tmp"
+	fi
+
+	echo "</article>" >> "$tmp"
+
+	page_content=$(get_content "$tmp")
+	>$tmp
+	build_page "$page_content"
+	mv $tmp "$g_build_path/$name.html"
+	chmod 644 "$g_build_path/$name.html"
+}
+
+get_tags()
+{
+	list=($1)
+
+	echo "<div class=\"tags\">"
+	echo "Tags: "
+	echo "<ul>"
+	for tag in "${list[@]}"; do
+		echo "<li>"
+		echo "$tag"
+		echo "</li>"
+	done
+	echo "</ul>"
+	echo "</div>"
+}
+
+build_all() {
+	for file in $(ls -d $g_posts_path/*.{html,md} 2>/dev/null); do
+		create_post $file
+	done
+}
+
+rebuild_index()
+{
+	local tmp_posts="$g_root/.sort"
+	local filename;
+	local tmp;
+	mkdir -p $tmp_posts
+	chmod 750 $tmp_posts
+	local files;
+
+	echo "Sorting posts..."
+	for file in $(ls -d $g_posts_path/*.{html,md} 2>/dev/null); do
+		name=$(get_basename $file)
+
+		get_file_parts "$file"
+		get_path_info "$file"
+		local datetime=${parts[g_POST_DATETIME]}
+
+		if [[ -z "$datetime" ]]; then
+			continue
+		fi
+
+		filename="${datetime//[:\-_+]/}_$name"
+		
+		local path="$tmp_posts/$filename.tmp"
+		echo "title: ${parts[g_POST_TITLE]}" >> "$path"
+		echo "datetime: ${parts[g_POST_DATETIME]}" >> "$path"
+		echo "tags: ${parts[g_POST_TAGS]}" >> "$path"
+		echo "${parts[g_POST_CONTENT]}" >> "$path"
+	done
+
+	echo "Generating posts index..."
+	tmp="$g_posts_path/index.tmp"
+	for file in $(ls -d $tmp_posts/*); do
+		get_file_parts "$file"
+		title=${parts[g_POST_TITLE]}
+		datetime=${parts[g_POST_DATETIME]}
+		tags=${parts[g_POST_TAGS]}
+		content=${parts[g_POST_CONTENT]}
+
+		file_name=$(get_basename "$file")
+		file_name=${file_name%.tmp}
+		get_path_info "$g_posts_path/$file_name"
+		name="${pathinfo[g_PATH_INFO_NAME]}"
+		ext="${pathinfo[g_PATH_INFO_EXT]}"
+		orig_filename=$(get_name "$(echo "$file_name" | cut -d_ -f2-)")
+
+		if [[ $ext == "md" ]]; then
+			content=$(convert_content "$content")
+		fi
+
+		echo "<article class=\"\">" >> "$tmp"
+		if [[ -z "$title" ]]; then
+			title="Untitled"
+		fi
+		echo "<h2><a href=\"$orig_filename.html\">$title</a></h2>" >> "$tmp"
+		echo "$content" >> "$tmp"
+		if [[ ! -z "$datetime" ]] || [[ ! -z "$tags" ]]; then
+			echo "<div class=\"meta\">" >> "$tmp"
+		fi
+		if [[ ! -z "$datetime" ]]; then
+			echo "Date: <time>$datetime</time>" >> "$tmp"
+		fi
+		if [[ ! -z "$tags" ]]; then
+			echo $(get_tags "$tags") >> "$tmp"
+		fi
+		if [[ ! -z "$datetime" ]] || [[ ! -z "$tags" ]]; then
+			echo "</div>" >> "$tmp"
+		fi
+		echo "</article>" >> "$tmp"
+		echo "<hr>" >> "$tmp"
+	done
+
+	page_content=$(get_content "$tmp")
+	>$tmp
+	build_page "$page_content"
+
+	mv $tmp "$g_build_path/index.html"
+	chmod 644 "$g_build_path/index.html"
+	rm -rf "$tmp_posts"
+}
+
+edit()
+{
+	local path="$g_posts_path/$1"
+	if [[ ! -f "$g_posts_path" ]]; then
+		mkdir -p "$g_posts_path"
+		chmod 744 "$g_posts_path"
+	fi
+	if [[ ! -f "$path" ]]; then
+		echo "title: " >> "$path"
+		echo "datetime: $(date -u +%FT%TZ)" >> "$path"
+		echo "tags: " >> "$path"
+		echo "" >> "$path"
+	fi
+
+	vim $path
+	create_post $path
+}
+
+if [[ ! -f "$g_build_path" ]]; then
+	mkdir -p "$g_build_path"
+	chmod 744 "$g_build_path"
+fi
+
+case $1 in
+	# Build all posts
+	"ba" )
+		if [[ -d "$g_posts_path" ]]; then
+			build_all
+			rebuild_index
+		else
+			echo "No posts"
+		fi
+	;;
+	
+	# Build single post
+	"b" )
+		if [[ -f "$g_posts_path/$2" ]]; then
+			create_post "$g_posts_path/$2"
+			rebuild_index
+		else
+			echo "Post not found: $g_posts_path/$2"
+		fi
+	;;
+
+	# Create/Edit posts
+	"c" )
+		edit $2
+		rebuild_index
+	;;
+esac
---
+++ posts/welcome.md
@@ -0,0 +1,42 @@
+title: Welcome 
+datetime: 2020-05-21T23:46:54Z
+tags: learning
+Welcome, this is my learning journal. The goal of this journal is to have a public place where I can write about my learning activities in a infrequently manner.
+
+To start this learning journal, the first thing I tried to learn was to make a simple site generator in bash. That script was used to build this page. I didn't make so many notes because I was expecting to write one, but let the code speak a bit for itself.
+
+It wasn't easy to create and at one point when things started to get uphill, I just stopped trying to make this in a "perfect" way, and starting to dump code just to make it work and this is how the first idea came on, not perfect, but actually working which was the intention.
+
+The code has some duplication code, and maybe not the best bash script out there. I tried to create this without following any tutorial or copying another generator, but I peek into the code of some projects to see how they do it, and I started from there just recalling how it did it, not making an exact copy and searching on google A LOT.
+
+I learned to use `man` and `grep` to find information in the `man` command, e.g. `man date | grep "format"`.
+
+I learned another thing, basically all the things in this code are just re-learned or learned for the first time. I was trying to remove the newline from the css when appending into the document, without no luck, tried `sed` with `[[:space]]`, didn't work because it removes the spaces too, yeah I know. I tried `[\n\r\t]` but it removes the `n`, `r`, and `t` characters from the styles.
+
+Then I remember, hey, early while working on this, there was a thing that  was annoying me, and it was `echo` removing newlines that I didn't want it to remove. Using `echo` without double quote remove the newlines. so there's that, I solve my issue without any regex or anything.
+
+	# From this
+	echo "<style>$styles</style>" >> "$tmp"
+
+	# To this
+	echo "<style>" >> "$tmp"
+	echo $styles >> "$tmp"
+	echo "</style>" >> "$tmp"
+
+Another thing I wanted to create was a variable that holds separate values to act like a struct/object, so I used an array, but there's something you have to keep in mind, if the variable is empty the value is not set in that position in the array, and ignored. For example, you want an array to always have three elements, each position must be wrapped in double-quote otherwise if the first variable is empty, the second variable now is going to be the first element, and the second will be defined by a third variable.
+
+	name="image"
+	ext=""
+	filename="$name$ext"
+	list=($name $ext $filename)
+
+If we want to `list[1]` to always be the extension value, but because `$ext` is empty that position is not set by `$ext`, but by `$filename`, to solve this each variable must be wrapped into double-quote.
+
+	name="image"
+	ext=""
+	filename="$name$ext"
+	list=("$name" "$ext" "$filename")
+
+I wanted to make this script barely functional first, then just fix things along the way (if I ever keep using it much), and add things that will make it easier for me to add notes. Because this static site generator was created with the purpose of learning and just create something.
+
+Back to the roots of having fun and enjoy making something. If interested the code available in [github](https://github.com/wellingguzman/b.sh).
